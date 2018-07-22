1c4df0990a9565f2c70d307719fa1e1643c4fa3a
Michal Pisanko <michal.pisanko@daisee.com>
2018-07-22 10:25:28 +1000

    fix incorrect condition

--- src/H3/Golf.hs
+++ src/H3/Golf.hs
@@ -10,17 +10,17 @@ skips :: [a] -> [[a]]
 skips xs = map (\i -> snd <$> filter ((==0).(`mod` i).fst) (zip [1 ..] xs)) [1 .. length xs]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima x@(_:y:z) = concatMap (\(a,b,c) -> bool [] [b] (2 * b > a + c)) $ zip3 x (y : z) z
+localMaxima x@(_:y:z) = concatMap (\(a,b,c) -> bool [] [b] (b > a && b > c)) $ zip3 x (y : z) z
 localMaxima _ = []
 -- localMaxima = catMaybes . unfoldr (case splitAt 3 of
---                                           ((a:b:[c]),t) -> bool (Just (Nothing, t)) (Just (Just b,t)) (2 * b > a + c)
+--                                           ((a:b:[c]),t) -> bool (Just (Nothing, t)) (Just (Just b,t)) (b > a && b > c)
 --                                           _ -> Nothing)
 
 hm :: [Integer] -> (Map Integer String)
 hm = foldr (adjust (++ "*")) (fromList (zip [0..9] (repeat "=")))
 -- hm :: [Integer] -> ((Map Integer String), Int)
 -- hm = foldr ((fst <$> (adjust (++ "*"))), (snd)) ((fromList (zip [0..9] (repeat "="))), 0)
--- hm = (map (head &&& length)) . group . sort
+-- hm = map (fromMaybe 0 . (flip lookup) $ (map (head &&& length)) . group . sort) [0..9]
 toString :: (Map Integer String) -> [String]
 toString m = map (\(i,n) -> (reverse (take (maximum . (map length) $ elems m) $ n ++ (repeat ' '))) ++ show i) $ assocs m
 histogram :: [Integer] -> String
